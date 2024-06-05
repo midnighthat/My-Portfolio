c2fc553bf6ebd24af213df3ec8276b6e11299762
amlasmilas <amlasmilas18@gmail.com>
2024-06-05 11:36:26 +0300

    My Portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const mune = document.querySelector('.menu-link');
+    const icon = document.querySelector('.hamburger-icon');
+    mune.classList.toggle('open');
+    icon.classList.toggle('open');
+}
